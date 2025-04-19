0790d21ada72f8b8ac5f7153f9d80aaa3c35700a
XYZboom <xyzboom@qq.com>
2025-04-19 14:53:48 +0800

    feat: support simpleName and qualifiedName by using compiler internal annotations

---
+++ src/commonTest/kotlin/KonstUtilsTest.kt
@@ -0,0 +1,12 @@
+package io.github.xyzboom.konst.ksp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class KonstUtilsTest {
+    @Test
+    fun konstUtilsShouldBeCorrect() {
+        assertEquals("KonstUtilsTest", simpleName<KonstUtilsTest>())
+        assertEquals("io.github.xyzboom.konst.ksp.KonstUtilsTest", qualifiedName<KonstUtilsTest>())
+    }
+}
